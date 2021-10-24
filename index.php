cea760432b0e4442b37f017dac10ca1df7e194a6
DucTai <ductai126@gmail.com>
2021-10-24 13:33:09 +0700

    some change

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
 		$_SESSION["username"]=$username;
     $_SESSION["usertype"] = "student";
 
-		header("location:StudentHomePage/index.php");
+		header("location:StudentHomePage");
 	}
 
 	elseif($row["usertype"]=="teacher"){
@@ -30,7 +30,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
 		$_SESSION["username"]=$username;
     $_SESSION["usertype"] = "teacher";
 
-		header("location:TeacherHomepage/index.php");
+		header("location:TeacherHomepage");
 	}
 
 	else{
